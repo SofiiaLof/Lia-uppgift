ed031d833ac9154aee4cd5cd7b9fe2d362e6d250
Sofiia Löf <sofiia.lof@utb.ecutbildning.se>
2022-04-01 00:27:44 +0200

    Fixade buggar med inloggning

--- script.js
+++ script.js
@@ -1,46 +1,67 @@
 $(".modal-wrapper").hide();
 
 $(document).ready(function(){
+
     const names = new Names();
 
     $(".login-button").click(function(){
         
-     if($(".modal-wrapper").is(':hidden')){
-      $(".modal-wrapper").show();
-      $(".main-page-container").addClass("blured");
-     }
+       let buttonValue = $(".login-button").text();
+
+       if(buttonValue === "Logga ut"){
+        $(".login-button").text("Logga in");
+         $(".welcome-text").hide();
+       }else{
+
+        $(".modal-wrapper").show();
+        $(".main-page-container").addClass("blured");
+        $(".main-header").addClass("blured");
+           
+       }
+       
     });
 
     $(".close-icon").click(function(){
         $(".modal-wrapper").hide();
         $(".main-page-container").removeClass("blured");
+        $(".main-header").removeClass("blured");
     });
 
     $("#submit-btn").click(function(){
+
+        $(".main-header").removeClass("blured");
+
         let inputValue = $("#name-input").val();
         let inputBold = $("<strong></strong>").text(inputValue);
         let pElement;
     
         let storage = JSON.parse(localStorage.getItem("userName"));
        
-
         if(storage === null){
 
-            pElement = $("<p></p>").text("Du är inloggad som ");
             names.addNames(inputValue);
+            pElement = $("<p></p>").text("Du är inloggad som ");
+            pElement.append(inputBold);
+            pElement.addClass("welcome-text");
+          
            
         }else{
 
             if(storage.includes(inputValue)){
                 pElement  = $("<p></p>").text("Välkommen tillbaka! Du är inloggad som ");
+                pElement.append(inputBold);
+                pElement.addClass("welcome-text");
             }
             else{
                 names.addNames(inputValue);
+                pElement = $("<p></p>").text("Du är inloggad som ");
+                pElement.append(inputBold);
+                pElement.addClass("welcome-text");
             }
             
         }
         
-        pElement.append(inputBold);
+      
         $(".main-login-button").append(pElement);
         $(".main-page-container").removeClass("blured");
         $(".modal-wrapper").hide();
